b6031e96c4fe04c6bad57621cc5791f81d90ab88
MRashkova <62338721+MRashkova@users.noreply.github.com>
2020-10-27 13:32:06 +0200

    Update app.js

--- Lab DOM Manipulations/01. List-Of-Items/app.js
+++ Lab DOM Manipulations/01. List-Of-Items/app.js
@@ -1,22 +1,9 @@
 function addItem() {
-    const items = document.getElementById("items");
-    const value = document.getElementById("newText").value;
- 
-    let newItem = document.createElement("li");
-    newItem.innerHTML = value;
- 
-    let deleteLink = document.createElement("a");
-    deleteLink.innerHTML = "[Delete]";
-    deleteLink.href = "#";
- 
-    deleteLink.addEventListener("click", function (e) {
-        e.preventDefault();
- 
-        this.parentNode.parentNode.removeChild(this.parentNode);
-    })
- 
-    newItem.appendChild(deleteLink);
-    items.appendChild(newItem);
- 
-    document.getElementById("newText").value = "";
+    let items = document.getElementById('items');
+    let input = document.getElementById('newItemText');
+    let liElement = document.createElement('li');
+
+    liElement.innerHTML = input.value;
+    input.value = '';
+    items.appendChild(liElement);
 }
