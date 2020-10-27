56c90b93286161942cf90caacb8962c838705ab0
MRashkova <62338721+MRashkova@users.noreply.github.com>
2020-10-27 13:33:42 +0200

    Update app.js

--- Lab DOM Manipulations/06. Highlight-Active/app.js
+++ Lab DOM Manipulations/06. Highlight-Active/app.js
@@ -1,3 +1,19 @@
-function focus() {
-    console.log('TODO:...');
+function deleteByEmail() {
+    let input = document.querySelector('input[name="email"]');
+    let enteredEmail = input.value;
+    //console.log(input.value);
+    let customerEmails = document.querySelectorAll('#customers td:nth-of-type(2)');
+    let isDeleted = false;
+    
+    for (const customerElement of customerEmails) {
+        if (customerElement.textContent === enteredEmail) {
+            customerElement.parentElement.remove();
+            isDeleted = true;
+        }
+    }
+
+    if (!isDeleted) {
+        let resultElement = document.getElementById('result');
+        resultElement.textContent = 'Not found.';
+    }
 }
