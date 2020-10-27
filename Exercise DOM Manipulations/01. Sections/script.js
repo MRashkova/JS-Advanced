e092508820ce60770799a95503c97b61c6e15c53
MRashkova <62338721+MRashkova@users.noreply.github.com>
2020-10-27 13:40:03 +0200

    Update script.js

--- Exercise DOM Manipulations/01. Sections/script.js
+++ Exercise DOM Manipulations/01. Sections/script.js
@@ -1,3 +1,24 @@
 function create(words) {
-   console.log('TODO:...');
+   let content = document.getElementById('content');
+   
+   words.forEach(word => {
+      let div = document.createElement('div');
+      content.appendChild(div);
+
+      let paragraph = document.createElement('p');
+      paragraph.textContent = word;
+      paragraph.style.display = 'none';
+
+      div.addEventListener('click', () => {
+         paragraph.style.display = "block";
+      })
+
+      div.appendChild(paragraph);
+      // paragraph.style.display = "none";
+
+      // paragraph.addEventListener('click')
+   });
+
+
+   
 }
