7222b25cb2eea65e282069cb2ba48184145b7f65
MRashkova <62338721+MRashkova@users.noreply.github.com>
2020-10-27 13:25:54 +0200

    Update solution.js

--- 05. JS-Advanced-DOM-Exercise-Resources/06. Numpad-Calculator/solution.js
+++ 05. JS-Advanced-DOM-Exercise-Resources/06. Numpad-Calculator/solution.js
@@ -1,4 +1,51 @@
 function solve() {
 
-    //TODO...
+    let expresion = document.getElementById('expressionOutput');
+    let result = document.getElementById('resultOutput');
+
+    document.querySelector('.keys').addEventListener('click', symbolClicked);
+    document.querySelector('.clear').addEventListener('click', clear);
+
+    function clear() {
+        expresion.textContent = '';
+        result.textContent = '';
+    }
+
+    function symbolClicked(event) {
+        let buttonPressed = event.target.value
+        switch (buttonPressed) {
+            case "/":
+            case "*":
+            case "+":
+            case "-":
+                expresion.textContent += ` ${buttonPressed} `;
+                break;
+            case "=":
+                let [leftOperand, operator, rightOperand] = expresion.textContent.split(' ');
+
+                if (!rightOperand  || !operator == undefined) {
+                    result.textContent = 'NaN';
+                } else {
+                   result.textContent = calculateResult(+leftOperand, operator, +rightOperand);
+                }
+                break;          
+            default:
+                expresion.textContent += buttonPressed;
+
+        }
+    }
+    function calculateResult(leftOperand, operator, rightOperand) {
+        switch (operator) {
+            case '*':
+                return leftOperand * rightOperand;
+            case '/':
+                return leftOperand / rightOperand;
+            case '-':
+                return leftOperand - rightOperand;
+            case '+':
+                return leftOperand + rightOperand;
+            
+        }
+    }
 }
+
