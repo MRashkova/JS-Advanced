cb287f79d8e92f9af4fd1dfb400e895996a2a1e9
MRashkova <62338721+MRashkova@users.noreply.github.com>
2020-10-27 14:07:13 +0200

    Create sortArray.js

---
+++ Exercise Advanced Functions/sortArray.js
@@ -0,0 +1,12 @@
+function solve(input, criteria) {
+    let sortFunction = {
+        asc: function (arr) {
+            return arr.sort((a, b) => a - b)
+        },
+        desc: function (arr) {
+            return arr.sort((a, b) => b - a)
+        }
+    } 
+    let func = sortFunction[criteria];
+    return func(input);
+}
