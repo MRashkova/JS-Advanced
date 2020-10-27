406df393d0e752ab4dfa57f319034946a1d4a26c
MRashkova <62338721+MRashkova@users.noreply.github.com>
2020-10-27 11:55:24 +0200

    Create System Components.js

---
+++ Exercise: Objects & Classes/System Components.js
@@ -0,0 +1,32 @@
+function solve(input) {
+    
+    let result = {};
+    input.forEach(line => {
+        let [systemName, component, subComponent] = line.split(' | ');
+
+        if (!result[systemName]) {
+            result[systemName] = {};
+        }
+
+        if (!result[systemName][component]) {
+            result[systemName][component] = [];
+        }
+        result[systemName][component].push(subComponent);
+    });
+
+    Object.entries(result).sort((curr, next) => {
+        return Object.entries(next[1]).length - Object.entries(curr[1]).length || curr[0].localeCompare(next[0]);
+    }).forEach(([system, components]) => {
+        console.log(system);
+        Object.entries(components).sort((curr, next) => {
+            return next[1].length - curr[1].length;
+        }).forEach(([component, subComponents]) => {
+            console.log(`|||${component}`);
+            subComponents.forEach(sub => {
+                console.log(`||||||${sub}`);
+            })
+          
+        })
+    })
+
+}
