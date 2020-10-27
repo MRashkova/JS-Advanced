7d0c7e512caf709a4aab63d87d2ea753d26e92c2
MRashkova <62338721+MRashkova@users.noreply.github.com>
2020-10-27 14:03:48 +0200

    Create currentFormat.js

---
+++ Lab Advanced Functions/currentFormat.js
@@ -0,0 +1,8 @@
+function dollarFormatterBuilder(currencyFormatter) {
+    let separator = ',';
+    let symbol = '$';
+    let symbolFirst = true;
+
+    let dollarFormatter =  currencyFormatter.bind(null, separator, symbol, symbolFirst);
+    return dollarFormatter;
+}
