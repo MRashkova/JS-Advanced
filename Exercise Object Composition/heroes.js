e6c50841ea655670d6ce03b17b3e47be249110a4
MRashkova <62338721+MRashkova@users.noreply.github.com>
2020-10-27 14:13:58 +0200

    Create heroes.js

---
+++ Exercise Object Composition/heroes.js
@@ -0,0 +1,30 @@
+function solve() {
+    const fighter = function (name) {
+        //различен ситаксис на същото, което го пише по-долу
+        //{this.name, this.health, this.stamina} = [name, 100, 100];
+        this.name = name;
+        this.health = 100;
+        this.stamina = 100;
+
+        this.fight = () => {
+            this.stamina--;
+            console.log(`${this.name} slashes at the foe!`);
+        }
+    }   
+    
+    const mage = function (name) {
+        this.name = name;
+        this.health = 100;
+        this.mana = 100;
+
+        this.cast = (spell) => {
+            this.mana--;
+            console.log(`${this.name} cast ${spell}`);
+        }
+    }
+
+    return {
+        fighter: (name = '') => new fighter (name),
+        mage: (name = '') => new mage (name)
+    }
+}
