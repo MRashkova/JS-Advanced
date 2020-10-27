8b1ec4a831f0a50bf7962519445642c9a8d71f73
MRashkova <62338721+MRashkova@users.noreply.github.com>
2020-10-27 13:25:37 +0200

    Update solution.js

--- 05. JS-Advanced-DOM-Exercise-Resources/05. Chat-Room/solution.js
+++ 05. JS-Advanced-DOM-Exercise-Resources/05. Chat-Room/solution.js
@@ -1,5 +1,19 @@
 function solve() {
-   //TODO...
+   let divElementContainingMessages = document.getElementById('chat_messages');
+   let button = document.getElementById('send');
+ 
+   button.addEventListener('click', () => {
+      let divElementMyMessage = document.getElementsByClassName('message my-message')[0];
+      let newDivElementMyMessage = divElementMyMessage.cloneNode(true);
+ 
+      newDivElementMyMessage.textContent = '';
+      let inputElement = document.getElementsByTagName('textarea')[0];
+ 
+      if (inputElement.value != undefined) {
+         newDivElementMyMessage.textContent = inputElement.value;
+         divElementContainingMessages.appendChild(newDivElementMyMessage);
+      }
+      inputElement.value = '';
+   });
+ 
 }
-
-
