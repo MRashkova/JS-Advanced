628fac13fe3003918fef2eb63e5b1a2a507edf8d
MRashkova <62338721+MRashkova@users.noreply.github.com>
2020-10-27 13:24:46 +0200

    Update dropdown.js

--- 05. JS-Advanced-DOM-Exercise-Resources/02. Fill-Dropdown/dropdown.js
+++ 05. JS-Advanced-DOM-Exercise-Resources/02. Fill-Dropdown/dropdown.js
@@ -1,3 +1,19 @@
 function addItem() {
-    console.log('TODO:...');
+
+    let text = document.getElementById('newItemText');
+    let data = document.getElementById('newItemValue');
+
+    let select = document.getElementById('menu');
+
+    let option2 = document.createElement('option');
+    option2.value = data.value;
+    option2.innerText = text.value;
+    let option = `<option value='${data.value}'>${text.value}</option>`
+
+    select.appendChild(option2);
+
+    text.value = '';
+    data.value = '';
+
+    // console.log('TODO:...');
 }
