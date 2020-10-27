fb31b19a93f9543a0b2e6132c8316af00723efbd
MRashkova <62338721+MRashkova@users.noreply.github.com>
2020-10-27 13:34:25 +0200

    Update app.js

--- Lab DOM Manipulations/03. Delete-From-Table/app.js
+++ Lab DOM Manipulations/03. Delete-From-Table/app.js
@@ -1,36 +1,19 @@
 function deleteByEmail() {
-    let input = document.getElementsByTagName('input')[0];
+    let input = document.querySelector('input[name="email"]');
+    let enteredEmail = input.value;
     //console.log(input.value);
-    let result = document.getElementById('result');
-
-    let eveEmail = document.getElementsByTagName('td')[1];
-    let nickEmail = document.getElementsByTagName('td')[3];
-    let didiEmail = document.getElementsByTagName('td')[5];
-    let tedyEmail = document.getElementsByTagName('td')[7];
-
+    let customerEmails = document.querySelectorAll('#customers td:nth-of-type(2)');
+    let isDeleted = false;
     
-    if (!input.value) {
-        result.textContent = 'Not found.', "Error message expected.";
-        console.log('Not found.');
-    } else if (input.value === 'eve@gmail.com') {
-        eveEmail.parentElement.remove();
-        result.textContent = 'Deleted.';
-        //console.log(input.value);
-    } else if (input.value === 'nick@yahooo.com') {
-        nickEmail.parentElement.remove();
-        result.textContent = 'Deleted.';
-        //console.log(input.value);
-    } else if (input.value === 'didi@didi.net') {
-        didiEmail.parentElement.remove();
-        result.textContent = 'Deleted.';
-        //console.log(input.value);
-    } else if (input.value === 'tedy@tedy.com') {
-        tedyEmail.parentElement.remove();
-        result.textContent = 'Deleted.';
-        //console.log(input.value);
-    } else {
-        result.textContent = 'Not found.', "Error message expected.";
-        console.log('Not found.');
+    for (const customerElement of customerEmails) {
+        if (customerElement.textContent === enteredEmail) {
+            customerElement.parentElement.remove();
+            isDeleted = true;
+        }
     }
-    // input.value = '';
+
+    if (!isDeleted) {
+        let resultElement = document.getElementById('result');
+        resultElement.textContent = 'Not found.';
+    }
 }
