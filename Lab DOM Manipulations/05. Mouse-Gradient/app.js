0fde91ba5543a559bdfa48ab6b8cefd62864cc97
MRashkova <62338721+MRashkova@users.noreply.github.com>
2020-10-27 13:33:29 +0200

    Update app.js

--- Lab DOM Manipulations/05. Mouse-Gradient/app.js
+++ Lab DOM Manipulations/05. Mouse-Gradient/app.js
@@ -1,3 +1,12 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    let gradient = document.getElementById('gradient');
+    let result = document.getElementById('result');
+
+    gradient.addEventListener('mousemove', (e) => {
+        const mouseX = e.offsetX
+        let position = Math.floor(Number(mouseX) / gradient.clientWidth * 100)
+
+        result.textContent = (`${parseInt(position)}%`)
+        console.log(position);
+    })
 }
