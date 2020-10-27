7298f78f22514e0c5773849fda7a4ae9e9d4e974
MRashkova <62338721+MRashkova@users.noreply.github.com>
2020-10-27 13:26:35 +0200

    Update solution.js

--- 05. JS-Advanced-DOM-Exercise-Resources/09. Shopping-Cart/solution.js
+++ 05. JS-Advanced-DOM-Exercise-Resources/09. Shopping-Cart/solution.js
@@ -1,3 +1,39 @@
 function solve() {
-   //TODO...
+    let button = document.getElementsByTagName("button")
+    let textArea = document.getElementsByTagName("textarea")[0]
+ 
+    // let list = {}
+    let list = new Map()
+ 
+    Array.from(button).forEach(b => {
+        b.addEventListener("click", (e) => {
+            if (e.target.className === "add-product") {
+                let currentElement = e.target.parentElement
+ 
+                let priceElement = currentElement.nextElementSibling.innerHTML
+                let brandElement = currentElement.previousElementSibling.children[0].innerHTML
+ 
+                // if (!list[brandElement]) {
+                if (!list.has(brandElement)) {
+                    // list[brandElement] = 0
+                    list.set(brandElement, 0)
+                }
+                // list[brandElement] += priceElement
+                list.set(brandElement, list.get(brandElement) + Number(priceElement))
+ 
+                textArea.value += `Added ${brandElement} for ${priceElement} to the cart.\n`
+            } else {
+ 
+                // let totalPrice = Number(Object.values(list).reduce((a,b)=>a+b))
+                let totalPrice = Number(Array.from(list.values()).reduce((a,b)=>+a+ +b, 0))
+                // textArea.innerHTML+=`You bought ${Object.keys(list).join(', ')} for ${totalPrice.toFixed(2)}.`
+                textArea.value+=`You bought ${Array.from(list.keys()).join(', ')} for ${totalPrice.toFixed(2)}.`
+ 
+                // Array.from(button).forEach(b => b.disabled = true)
+                let buttons = Array.from(document.querySelectorAll('button'));
+                buttons.forEach(button => button.disabled = true);
+ 
+            }
+        })
+    })
 }
